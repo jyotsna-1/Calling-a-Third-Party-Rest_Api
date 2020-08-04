68a2de400da6dfe5c251035247d81f3307dbdef5
Enfec Technologies <Enfec Technologies@LAPTOP-1H59U3NT>
2020-08-04 17:00:04 +0530

    First Commit

---
+++ CallingRestApi/src/main/java/com/example/model/Address.java
@@ -0,0 +1,16 @@
+package com.example.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+	
+	private Geo geo;
+}
---
+++ CallingRestApi/src/main/java/com/example/model/Company.java
@@ -0,0 +1,15 @@
+package com.example.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+	private String name;
+}
